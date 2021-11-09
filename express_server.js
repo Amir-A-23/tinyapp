eb4cbfa31dd92c5e0766f4e20ffb4f54af87aeed
Amir <amir.a.23@hotmail.com>
2021-11-09 18:38:52 +0000

    Parse new url and log the POST body

--- express_server.js
+++ express_server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+
+//use body-parser to translate body buffer into readable string data
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const PORT = 8080; // default port 8080
 
 //set view engine to ejs
 app.set("view engine", "ejs");
-
 
 const urlDatabase = { //list of urls
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +25,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 }); 
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls/:shortURL", (req, res) => {
   const short = req.params.shortURL; //use req.params to get string of :shortURL in the url, whatever the input, stored it in short
   const templateVars = { shortURL: short, longURL: urlDatabase[short] }; //the short form is short, the long url is the value of the key "short" in the url DB
@@ -31,15 +42,16 @@ app.get("/urls/:shortURL", (req, res) => {
   //res.send(req.params);
   res.render("urls_show", templateVars);
 });
-
+/*
 app.get("/urls.json", (req, res) => { 
   res.json(urlDatabase);
 });
-
+*/
+/*
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n"); //sent a response to be rendering in client broswer as html
 });
-
+*/
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
