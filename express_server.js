5df751a5b681cca1500c29ab7ca4c73506badda1
Amir <amir.a.23@hotmail.com>
2021-11-09 01:13:51 +0000

    used req.param to get the short and long form urls, created urls_show to show both urls

--- express_server.js
+++ express_server.js
@@ -11,13 +11,25 @@ const urlDatabase = { //list of urls
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
-});
 
 app.get("/", (req, res) => { //  / is the root path
   res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+}); 
+
+app.get("/urls/:shortURL", (req, res) => {
+  const short = req.params.shortURL;
+  const templateVars = { shortURL: short, longURL: urlDatabase[short]/* What goes here? */ };
+  console.log("Helllllllllo", templateVars);
+  console.log("Req Params", req.params);
+  console.log(short);
+
+  //res.send(req.params);
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => { 
